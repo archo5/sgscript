9cbd96bda9dc9ca417b28b0c6e05a79fb28d2c5c
snake5 <snake5creator@gmail.com>
2013-08-17 09:42:06 +0300

    absolute paths for dy.libs + doc. update with language ref. stub

--- src/sgs_xpc.c
+++ src/sgs_xpc.c
@@ -9,8 +9,10 @@
 #  define NOMCX
 #  define NOMINMAX
 #  include <windows.h>
+#  define SGS_MAX_PATH 4096
 #elif defined(__linux__)
 #  include <dlfcn.h>
+#  define SGS_MAX_PATH PATH_MAX
 #endif
 
 #include "sgs_xpc.h"
@@ -21,6 +23,15 @@ int sgs_GetProcAddress( const char* file, const char* proc, void** out )
 #ifdef WIN32
 	HMODULE mod;
 	UINT pe;
+	char abspath[ SGS_MAX_PATH + 1 ];
+	DWORD ret;
+	
+	ret = GetFullPathNameA( file, SGS_MAX_PATH, abspath, NULL );
+	if( ret > 0 && ret < SGS_MAX_PATH )
+	{
+		abspath[ SGS_MAX_PATH ] = 0;
+		file = abspath;
+	}
 
 	pe = SetErrorMode( SEM_FAILCRITICALERRORS );
 	mod = LoadLibraryA( file );
@@ -35,7 +46,16 @@ int sgs_GetProcAddress( const char* file, const char* proc, void** out )
 	return 0;
 
 #elif defined(__linux__)
-	void* lib = dlopen( file, RTLD_NOW );
+	void* lib;
+	char* abspath[ SGS_MAX_PATH + 1 ];
+	
+	if( realpath( file, abspath ) )
+	{
+		abspath[ SGS_MAX_PATH ] = 0;
+		file = abspath;
+	}
+	
+	lib = dlopen( file, RTLD_NOW );
 	if( !lib )
 		return SGS_XPC_NOFILE;
 
