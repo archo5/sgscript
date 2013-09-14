2d63816c405c532ccb17244b8217c261ec56e3cb
snake5 <snake5creator@gmail.com>
2013-09-14 12:21:38 +0300

    improved sgs_GetTime precision on Linux

--- src/sgs_util.c
+++ src/sgs_util.c
@@ -350,8 +350,14 @@ void ht_iterate( sgs_HashTable* T, sgs_HTIterFunc func, void* userdata )
 
 double sgs_GetTime()
 {
+#ifdef __linux
+	struct timespec ts;
+	clock_gettime( CLOCK_MONOTONIC, &ts );
+	return (double) ts.tv_sec + 0.000000001 * (double) ts.tv_nsec;
+#else
 	clock_t clk = clock();
 	return (double)( clk ) / (double)( CLOCKS_PER_SEC );
+#endif
 }
 
 
